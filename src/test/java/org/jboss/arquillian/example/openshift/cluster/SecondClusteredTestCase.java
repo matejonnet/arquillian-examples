f986bcae0fe1bcbd6f85700ab40c66bdc141e90e
Matej Lazar <matejonnet@gmail.com>
2012-12-04 00:19:47 +0100

    Arquilian sample on OpenShift cluster.

---
+++ src/test/java/org/jboss/arquillian/example/openshift/cluster/SecondClusteredTestCase.java
@@ -0,0 +1,63 @@
+package org.jboss.arquillian.example.openshift.cluster;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:mlazar@redhat.com">Matej Lazar</a>
+ */
+@RunWith(Arquillian.class)
+public class SecondClusteredTestCase {
+
+    @Deployment (name = "dep1") @TargetsContainer("openshift-node1")
+    public static WebArchive getDeploymentA() {
+        return getDeployment();
+    }
+
+    @Deployment(name = "dep2") @TargetsContainer("openshift-node2")
+    public static WebArchive getDeploymentB() {
+        return getDeployment();
+    }
+
+    public static WebArchive getDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "cluster-tests2.war")
+            .setWebXML("web.xml")
+            .addAsManifestResource("jboss-deployment-structure.xml");
+    }
+
+    @InSequence(10)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void testCacheOnDepA() {
+        System.out.println("Running test " + this.getClass().getName() + " on dep1.");
+    }
+
+    @InSequence(20)
+    @Test
+    @OperateOnDeployment("dep2")
+    public void testCacheOnDepB() {
+        System.out.println("Running test " + this.getClass().getName() + " on dep2.");
+    }
+
+    @InSequence(30)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void testCacheOnDepA1() {
+        System.out.println("Running 2nd test " + this.getClass().getName() + " on dep1.");
+    }
+
+    @InSequence(40)
+    @Test
+    @OperateOnDeployment("dep2")
+    public void testCacheOnDepB1() {
+        System.out.println("Running 2nd test " + this.getClass().getName() + " on dep2..");
+    }
+
+}
---
+++ src/test/java/org/jboss/arquillian/example/openshift/cluster/SimpleClusteredTestCase.java
@@ -0,0 +1,67 @@
+package org.jboss.arquillian.example.openshift.cluster;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:mlazar@redhat.com">Matej Lazar</a>
+ */
+@RunWith(Arquillian.class)
+public class SimpleClusteredTestCase {
+
+    @Deployment (name = "dep1") @TargetsContainer("openshift-node1")
+    public static WebArchive getDeploymentA() {
+        return getDeployment();
+    }
+
+    @Deployment(name = "dep2") @TargetsContainer("openshift-node2")
+    public static WebArchive getDeploymentB() {
+        return getDeployment();
+    }
+
+    public static WebArchive getDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "cluster-tests.war")
+            .setWebXML("web.xml")
+            .addAsManifestResource("jboss-deployment-structure.xml");
+    }
+
+    @InSequence(10)
+    @Test
+    @OperateOnDeployment("dep1")
+    //@TargetsContainer("openshift-node1")  //TODO why is TargetsContainer used in ServletURIHandler.locateHTTPContext,it could be read from deployment
+    public void testCacheOnDepA() {
+        System.out.println("Running test on dep1.");
+    }
+
+    @InSequence(20)
+    @Test
+    @OperateOnDeployment("dep2")
+    //@TargetsContainer("openshift-node2")
+    public void testCacheOnDepB() {
+        System.out.println("Running test on dep2.");
+    }
+
+    @InSequence(30)
+    @Test
+    @OperateOnDeployment("dep1")
+    //@TargetsContainer("openshift-node1")
+    public void testCacheOnDepA1() {
+        System.out.println("Running 2nd test on dep1.");
+    }
+
+    @InSequence(40)
+    @Test
+    @OperateOnDeployment("dep2")
+    //@TargetsContainer("openshift-node2")
+    public void testCacheOnDepB1() {
+        System.out.println("Running 2nd test on dep2..");
+    }
+
+}
