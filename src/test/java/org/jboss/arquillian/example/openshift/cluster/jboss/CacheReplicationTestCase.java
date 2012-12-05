da1c6286e5cb96b862b5f45b4721605c1d2b2159
Matej Lazar <matejonnet@gmail.com>
2012-12-05 12:21:27 +0100

    JBoss AS replication test.

---
+++ src/test/java/org/jboss/arquillian/example/openshift/cluster/jboss/CacheReplicationTestCase.java
@@ -0,0 +1,124 @@
+package org.jboss.arquillian.example.openshift.cluster.jboss;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:mlazar@redhat.com">Matej Lazar</a>
+ */
+@RunWith(Arquillian.class)
+public class CacheReplicationTestCase {
+
+    //@Resource(mappedName="java:jboss/infinispan/container/capedwarf")
+    @Resource(mappedName="java:jboss/infinispan/container/cluster")
+    CacheContainer container;
+
+    private Cache<String, String> cache;
+
+    private Cache<String, String> getCache() {
+        if (cache == null) {
+            cache = container.getCache("search");
+        }
+        return cache;
+    }
+
+
+    /**
+     * Wake-up cache on first node.
+     */
+    @InSequence(10)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void wakeUpCacheA() throws IOException {
+        getCache();
+    }
+
+    /**
+     * Wake-up cache on second node.
+     */
+    @InSequence(20)
+    @Test
+    @OperateOnDeployment("dep2")
+    public void wakeUpCacheB() throws IOException {
+        getCache();
+    }
+
+
+    @InSequence(100)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void testCacheOnDepA() {
+        waitForSync();
+        System.out.println("Running test on dep1.");
+        getCache().put("key", "value");
+    }
+
+    @InSequence(110)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void readFromCacheOnDepA() {
+        waitForSync();
+        System.out.println("Running test on dep1.");
+        String value = getCache().get("key");
+        Assert.assertEquals("value", value);
+    }
+
+    @InSequence(120)
+    @Test
+    @OperateOnDeployment("dep2")
+    public void readFromCacheOnDepB() {
+        waitForSync();
+        System.out.println("Running test on dep2.");
+        String value = getCache().get("key");
+        Assert.assertEquals("value", value);
+    }
+
+    @InSequence(1000)
+    @Test
+    @OperateOnDeployment("dep1")
+    public void cleanUp() throws IOException {
+        waitForSync();
+        getCache().clear();
+    }
+
+    @Deployment (name = "dep1") @TargetsContainer("openshift-node1")
+    public static WebArchive getDeploymentA() {
+        return getDeployment();
+    }
+
+    @Deployment(name = "dep2") @TargetsContainer("openshift-node2")
+    public static WebArchive getDeploymentB() {
+        return getDeployment();
+    }
+
+    public static WebArchive getDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "cache-repl.war")
+            .setWebXML("web.xml")
+            .addAsManifestResource("jboss-deployment-structure.xml");
+    }
+
+    private void waitForSync() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+}
